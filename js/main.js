0f13028b1b1ae89d3f1fd7ceaa5bef7313f807cb
Ty <Ty@Tys-MacBook-Pro.local>
2018-04-08 17:40:17 -0400

    Added frontend stuff

---
+++ js/main.js
@@ -0,0 +1,28 @@
+$(document).ready(() => {
+  //change timer
+  //brown timer starts active
+  $("#timer-brown-button").click(function(event) {
+    event.preventDefault();
+    $("#timer-other-form").addClass("non-active-timer");
+    $("#timer-brown-form").removeClass("non-active-timer");
+  });
+
+  $("#timer-other-button").click(function(event) {
+    event.preventDefault();
+    $("#timer-brown-form").addClass("non-active-timer");
+    $("#timer-other-form").removeClass("non-active-timer");
+  });
+
+
+  //header motion
+  var headerHeight = $("header").height();
+  console.log(headerHeight);
+  $(document).on("scroll",function() {
+    if ($(document).scrollTop() > headerHeight) {
+      $("header").addClass("header-scroll");
+    } else {
+      $("header").removeClass("header-scroll");
+    }
+  });
+
+});
