73317b070411d7ea2a2044c2d6258bc1738f325f
Nick Faulkner <nmfaulkner@gmail.com>
2018-04-30 15:09:04 -0400

    google sign-in ish

--- public/js/main.js
+++ public/js/main.js
@@ -348,6 +348,14 @@ function printEmptyQueuePage() {
   console.log("TBD");
 }
 
+function onSignIn(googleUser) {
+  console.log("hello");
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
 
 //make timeremaingin function
 //make lasercutter assignment fucntion
