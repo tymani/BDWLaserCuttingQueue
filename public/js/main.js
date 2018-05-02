12acd59cdaec13eb00ddfc6eef3f7c7caba6d728
Nick Faulkner <nmfaulkner@gmail.com>
2018-05-01 20:20:39 -0400

    sign in works

--- public/js/main.js
+++ public/js/main.js
@@ -376,16 +376,5 @@ function printEmptyQueuePage() {
 //make lasercutter assignment fucntion
 //every client is updated with who currently at the
 
-function onSignIn(googleUser) {
-  console.log("worked!!!");
-  var profile = googleUser.getBasicProfile();
-  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-  console.log('Name: ' + profile.getName());
-  console.log('Image URL: ' + profile.getImageUrl());
-  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-}
-
-
-
 
 });
