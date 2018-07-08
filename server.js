38cb1b1fa3299d2c70002aeba931bb264debd8ce
Ty <Ty@Tys-MacBook-Pro.local>
2018-07-08 11:20:31 -0700

    continued styling

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const xoauth2 = require('xoauth2');
 
 var io = require('socket.io').listen(server);
 server.listen(8080, function () {
-  console.log("Listening on port 8080")
+  console.log("Listening on port 8080");
 });
 
 app.use("/", express.static(__dirname));
